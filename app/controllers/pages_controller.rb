a89073caa6828fc9aea78935bdfe65b5ebd96882
Steve <stevemargc@gmail.com>
2012-01-29 12:05:59 -0500

    title

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,18 @@
 class PagesController < ApplicationController
   def home
+    @title = "Lighthouse Benefits"
   end
 
   def about
+    @title = "About - Lighthouse Benefits"   
   end
 
   def solutions
+    @title = "Solutions - Lighthouse Benefits"
   end
 
   def contact
+    @title = "Contact - Lighthouse Benefits"
   end
 
 end
